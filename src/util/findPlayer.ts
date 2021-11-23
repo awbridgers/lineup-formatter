8c8313c795cd9ef9c56ba1dec31a78670c705efd
Adam Bridgers <awbridgers@users.noreply.github.com>
2021-11-22 20:58:42 -0500

    fix issue with results being in all lowercase

--- src/util/findPlayer.ts
+++ src/util/findPlayer.ts
@@ -3,20 +3,19 @@ import {roster} from '../roster';
 export const findPlayer = (firstName: string, lastName: string): string => {
   //break roster into first and last names
   const splitRoster = roster.map((name) => {
-    const lower = name.toLowerCase();
-    const [first, ...rest] = lower.split(' ');
+    const [first, ...rest] = name.split(' ');
     const last = rest.join(' ');
     return {first, last};
   });
   //now check the last names
-  const check = splitRoster.filter((x) => x.last === lastName);
+  const check = splitRoster.filter((x) => x.last.toLowerCase() === lastName);
   if (check.length === 1) {
     //1 name, must be the player
     return `${check[0].first} ${check[0].last}`;
   } else {
     //need to check first name
     const firstNameCheck = check.filter((x) => {
-      const start = x.first.substr(0, firstName.length);
+      const start = x.first.substr(0, firstName.length).toLowerCase();
       return start === firstName;
     });
     if (firstNameCheck.length === 1) {
